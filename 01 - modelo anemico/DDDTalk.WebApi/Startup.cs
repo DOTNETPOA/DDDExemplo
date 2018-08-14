f38863df91f0d87eb31002c3622562c193bd86f1
Gabriel Schmitt Kohlrausch <gabriel@society.com.br>
2018-08-14 20:25:18 -0300

    configuração de service provider do asp core

--- 01 - modelo anemico/DDDTalk.WebApi/Startup.cs
+++ 01 - modelo anemico/DDDTalk.WebApi/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DDDTalk.WebApi.Helpers;
+using DDDTalk.WebApi.Infra;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -21,13 +23,15 @@ namespace DDDTalk.WebApi
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(new AppSettingsHelper(Configuration));
+            services.AddScoped<AlunosRepositorio>();
+            services.AddScoped<TurmasRepositorio>();
+            services.AddScoped<InscricoesRepositorio>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
